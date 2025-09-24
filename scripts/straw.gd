0ff862af6dc53dfb4cb60d62cffb37b925d5b374
audreycharge <64012818+audreycharge@users.noreply.github.com>
2025-09-24 12:00:54 -0400

    Initial commit

---
+++ scripts/straw.gd
@@ -0,0 +1,15 @@
+class_name Straw extends MeshInstance2D
+
+const SPEED = 100.0
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	pass
