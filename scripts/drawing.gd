5271f3ef3be07ed40e9a8f71900c681ec951bd40
multicolorcolor <112100252+multicolorcolor@users.noreply.github.com>
2025-09-25 13:03:12 -0400

    heart_sauce

---
+++ scripts/drawing.gd
@@ -0,0 +1,17 @@
+extends Node2D
+@onready var lines: Node2D = $Line2D
+var pressed:bool = false
+var current_line: Line2D = null
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			pressed = event.pressed
+			
+			if pressed:
+				current_line = Line2D.new()
+				current_line.default_color = Color.BROWN
+				current_line.width = 4
+				lines.add_child(current_line)
+				current_line.add_point(event.position)
+	
