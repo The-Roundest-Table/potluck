334520e63dc74ce2bf1b804ca17b2a3d3547df11
audreycharge <64012818+audreycharge@users.noreply.github.com>
2025-09-25 13:00:48 -0400

    idk
    made a tomato object that will disappear when in contact with the frying pan

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -1,10 +1,11 @@
-class_name Straw extends MeshInstance2D
+class_name Player_Movement extends Node
 
-const SPEED = 100.0
+const SPEED = 300.0
+var speed = 1;
 @onready var parent: CharacterBody2D = get_parent()
 
-var start_position
-var end_position
+var alive = true;
+
 
 
 
@@ -17,3 +18,20 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	
 	pass
+	
+func _physics_process(delta: float) -> void:
+	#get input to move the player
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		parent.velocity.x = direction * SPEED * speed
+	else:
+		parent.velocity.x = move_toward(parent.velocity.x, 0, SPEED*speed)
+	parent.move_and_slide();
+	handleCollision();
+	
+
+func handleCollision():
+	for i in parent.get_slide_collision_count():
+		var collision = parent.get_slide_collision(i);
+		var collider = collision.get_collider();
+		print(collider.name)
