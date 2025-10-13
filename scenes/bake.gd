8bc1b1ba2b345f2e794d4d701fe00bb4d5ec029e
audreycharge <64012818+audreycharge@users.noreply.github.com>
2025-10-12 21:04:22 -0400

    finished boba tea scene
    got the straw to fall into the boba cup if you click with the mouse yippee! mostly i was spending time trying to figure out how to duplicate the nastar so that we can bake a bunch of them at once

--- scenes/bake.gd
+++ scenes/bake.gd
@@ -1,10 +1,24 @@
-class_name BakeScene extends BaseScene
+extends BaseScene
+
+@onready var tart_copies: Node2D = $tart_copies
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super()
-	pass # Replace with function body.
+	#print(tart)
+	for i in tart.get_children():
+		tart_copies.add_child(i.duplicate());
+	tart.visible = false
+	#print(tart_copies.get_children())
+	for i in 3:
+		for j in 3:
+			var newtart = tart_copies.duplicate()
+			print(str(i,newtart.position))
+			add_child(newtart)
+			newtart.set_position(Vector2(170*i+ 400,100*j+ 200))
+			#newtart.set_global_position(get_viewport().get_visible_rect().size)
+	tart_copies.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
---
+++ scenes/straw.gd
@@ -0,0 +1,53 @@
+class_name straw extends CharacterBody2D
+
+
+const SPEED = 500.0
+const JUMP_VELOCITY = -400.0
+var drop = false
+@export var limit = 10
+@export var endPoint: Marker2D
+
+var startPosition
+var endPosition
+
+func _ready():
+	startPosition = position
+	endPosition = endPoint.global_position
+	
+func changeDirection():
+	var tempEnd = endPosition
+	endPosition = startPosition
+	startPosition = tempEnd
+
+func updateVelocity():
+	var moveDirection = endPosition - position
+	if moveDirection.length() < limit:
+		print("hi")
+		changeDirection()
+	velocity.x = moveDirection.normalized().x * SPEED
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor() and drop:
+		
+		velocity += get_gravity() * delta *2
+
+
+	#velocity.x = direction * SPEED
+	if !drop:
+		updateVelocity()
+	else:
+		velocity.x = 0
+	move_and_slide()
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
+		drop = true
+
+
+func _on_fall_body_entered(body: Node2D) -> void:
+	#print("missed")
+	if body is straw:
+		drop = false
+		velocity.y = 0
+		position = startPosition;
