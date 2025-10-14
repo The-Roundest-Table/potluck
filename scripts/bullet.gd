0a6a01577f616fecbde86dadae6b4beed5a6a8b5
multicolorcolor <112100252+multicolorcolor@users.noreply.github.com>
2025-10-14 14:36:11 -0400

    tower

---
+++ scripts/bullet.gd
@@ -0,0 +1,15 @@
+extends Area2D
+ 
+var direction : Vector2 = Vector2.RIGHT
+var speed : float = 300
+ 
+func _physics_process(delta):
+	position += direction * speed * delta
+ 
+ 
+func _on_screen_exited():
+	queue_free()
+
+
+func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
+	queue_free()
