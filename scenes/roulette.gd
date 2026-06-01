d6607dc0d82de723bb4c650ffbc57f2ddb79a91b
audreycharge <64012818+audreycharge@users.noreply.github.com>
2026-06-01 13:23:04 -0400

    updates
    - fixed roulette to inlude other dishes
    - made ui changes to soysauce

--- scenes/roulette.gd
+++ scenes/roulette.gd
@@ -35,32 +35,17 @@ func load_json_file():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("book"):
-#		note: please fix, cookbook can't be opened
 		scene_manager.open_cookbook(self)
-		#get_tree().call_deferred('change_scene_to_file', "res://GUI/cookbook.tscn")
-	#if popup.opened:
-		#print_debug("close please")
-	#$round.text = str("Round ", scene_manager.round + 1)
 	$turn.text = str("Turn ", scene_manager.turn + 1)
 
 
 func _on_spinner_selection(choice) -> void:
-	#print_debug(choice)
-	#print_debug(rounds[scene_manager.round][choice])
-	#var item = rounds[scene_manager.round][choice]
 	var item;
 	if scene_manager.turn != 3:
 		item = scene_manager.regular_food[choice]
 		scene_manager.regular_food.pop_at(choice)
 	else:
-		item = special_food[choice];
-	#var id = scene_manager.round*4 + choice
-	#if scene_manager.cookbook_array.has(choice):
-	#scene_manager.update_cookbook(food_descs.get(item),id)
-	#var item = 'sate'
-	#if "heartcatcher nastar boba kyorchekh".contains(item):
-		#scene_manager.change_scene(self, item);
-	#else:
+		item = special_food[choice % 4];
 	var name = food_descs.get(item).name
 	var desc = food_descs.get(item).description
 	var art = food_descs.get(item).art
--- scripts/drawing.gd
+++ scripts/drawing.gd
@@ -7,6 +7,7 @@ class_name Heart_sauce extends BaseScene
 var sauce:bool = true
 var pressed:bool = false
 var current_line: Line2D = null
+var sauce_on:bool = false;
 
 
 func _ready():
@@ -17,32 +18,33 @@ func _ready():
 	#Input.set_custom_mouse_cursor(load("res://assets/placeholder/soy-sauce-peking-duck-kikkoman-bottle-bottle-f70ef99f0ea6d017a9d13fd3a746bb3f.png"))
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			pressed = event.pressed
-			
-			if pressed && sauce == true:
-				animator.play("sauce_dripping_in")
-				current_line = Line2D.new()
-				#current_line.default_color = Color.BROWN
-				current_line.width = 50
-				current_line.texture_mode = Line2D.LINE_TEXTURE_STRETCH
-				current_line.texture = Brush
-				lines.add_child(current_line)
-				current_line.add_point(event.position)
-			if event.is_released() && sauce == true:
-				animator.play("sauce_dripping_out")
-			if pressed && sauce == false:
-				pass
-	elif  event is InputEventMouseMotion and pressed:
-		current_line.add_point(event.position)
+	if sauce_on:
+		if event is InputEventMouseButton:
+			if event.button_index == MOUSE_BUTTON_LEFT:
+				pressed = event.pressed
+				
+				if pressed && sauce:
+					animator.play("sauce_dripping_in")
+					current_line = Line2D.new()
+					#current_line.default_color = Color.BROWN
+					current_line.width = 50
+					current_line.texture_mode = Line2D.LINE_TEXTURE_STRETCH
+					current_line.texture = Brush
+					lines.add_child(current_line)
+					current_line.add_point(event.position)
+				if event.is_released() && sauce:
+					animator.play("sauce_dripping_out")
+				if pressed && !sauce:
+					pass
+		elif event is InputEventMouseMotion and pressed:
+			current_line.add_point(event.position)
 
 
 func _on_scallions_button_pressed() -> void:
-	sauce = false
+	#sauce = false
 	Dialogic.start('heartsauce', 'scallions')
 	$ScallionsButton.disabled = true
-	current_line.width = 0
+	#current_line.width = 0
 	#lines.visible = false
 	#scene_manager.change_scene(self, connected_scene)
 
@@ -55,3 +57,14 @@ func _on_dialogic_signal(argument:String):
 	if argument == "scallions_sprinkle":
 		animator.play("scallions")
 		#sauce = false
+
+
+func _on_texture_button_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$TextureButton/bottle.visible = false
+		$SoySauce.visible = true
+		sauce_on = true
+	else:
+		$TextureButton/bottle.visible = true
+		$SoySauce.visible = false
+		sauce_on = false
